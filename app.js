c8f547b37469d957b52e13bfd0fa506fcfa41028
nate <nathanmasters.com@hotmail.com>
2021-05-06 22:37:56 +0100

    tidied up code and added about me

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
-const navSlide  = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
 
-    burger.addEventListener('click', () => {
-        nav.classList.toggle('nav-active');
-        navLinks.forEach((link, index) => {
-            if (link.getElementsByClassName.animation) {
-                link.getElementsByClassName.animation = '';
-            }  else {
-                link.style.animation = `navLinkFade 0.5 ease forwards ${index / 7 + 1.5}s`
-            }  
-        })
-        burger.classList.toggle('toggle');
-    })
-    
-}
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+    navLinks.forEach((link, index) => {
+      if (link.getElementsByClassName.animation) {
+        link.getElementsByClassName.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5 ease forwards ${
+          index / 7 + 1.5
+        }s`;
+      }
+    });
+    burger.classList.toggle("toggle");
+  });
+};
 
 navSlide();
